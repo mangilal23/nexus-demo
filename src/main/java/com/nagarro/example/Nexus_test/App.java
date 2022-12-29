4fe7a1ffe5a569f13dbc688d90ee51995353bc12
Mangi Lal <Mangi.Lal@partner.bmwgroup.com>
2022-12-29 15:03:31 +0530

    added first step

---
+++ src/main/java/com/nagarro/example/Nexus_test/App.java
@@ -0,0 +1,13 @@
+package com.nagarro.example.Nexus_test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
